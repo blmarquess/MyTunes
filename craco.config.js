61ce0336f963737ef20d0b6ad9694e196fffe1c8
Bruno <blmarques.dev@gmail.com>
2021-11-13 21:46:00 -0300

    TailWindCSS setup done

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+// craco.config.js
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+};
