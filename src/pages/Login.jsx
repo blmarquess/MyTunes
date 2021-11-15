6fc1fc77d3a37d74d9e324c8d05b17c7de800357
Bruno <blmarques.dev@gmail.com>
2021-11-14 21:20:07 -0300

    Style: Login Page stylezed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const imgFon = 'https://images.vexels.com/media/users/3/203199/isolated/preview/93e9f7dbfc842de939011bc64aa482b6-fones-de-ouvido-totalmente-verdes.png';
+
 export default class Login extends React.Component {
   constructor() {
     super();
@@ -22,29 +24,33 @@ export default class Login extends React.Component {
 
   render() {
     return (
-      <div
-        data-testid="page-login"
-        className="mx-auto mt-4 flex justify-center items-center w-11/12"
-      >
-        <div className="mx-auto grid col-1">
-          <h1>&#127926; Project TribTunes &#127911; </h1>
-          <img src="#" alt="img_logo" />
-        </div>
-        <div className="container flex justify-center items-center">
-          <imput
-            type="text"
-            data-testid="login-name-input"
-            className="p-4 rounded-3xl w-full bg-white border-2"
-            onChange={ this.handleImputChange }
-          />
-          <button
-            data-testid="login-submit-button"
-            type="button"
-            className="bg-blue-600 border-2 rounded-3xl px-4 py-2 text-white"
-            onClick={ this.onClickLogin }
-          >
-            Entrar
-          </button>
+      <div className="m-auto p-20 flex h-full w-full items-center justify-center">
+        <div
+          data-testid="page-login"
+          className="m-auto h-4/5 w-4/5 flex shadow-2xl p-16 rounded-3xl"
+        >
+          <div className="m-auto grid grid-cols-1 text-center">
+            <h1>&#127926; Project TribTunes &#127911; </h1>
+            <img src={ imgFon } alt="img_logo" className="m-auto w-3/5 text-blue-700" />
+          </div>
+          <div className="mx-auto flex justify-center items-center">
+            <imput
+              type="text"
+              data-testid="login-name-input"
+              className="p-5 rounded-3xl w-96 bg-white border-2 mx-2
+              text-gray-400 place-content-start"
+              placeholder="Enter your name"
+              onChange={ this.handleImputChange }
+            />
+            <button
+              data-testid="login-submit-button"
+              type="button"
+              className="bg-green-700 border-2 rounded-3xl px-8 py-2 text-white"
+              onClick={ this.onClickLogin }
+            >
+              Entrar
+            </button>
+          </div>
         </div>
       </div>
     );
