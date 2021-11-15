320b96e6b5b3605249227f7f265bb75397d92305
Bruno <blmarques.dev@gmail.com>
2021-11-15 04:58:33 -0300

    Implement: search imput and button on Search component

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import Header from '../components/Header';
 
-export default () => (
-  <div>
-    <Header />
-    <div data-testid="page-search">
-      To no search! Whit Header.
-    </div>
-  </div>
-);
+export default class Search extends React.Component {
+  constructor() {
+    super();
+    this.onChangeInputsearch = this.onChangeInputsearch.bind(this);
+    this.onChangeInputsearch = this.onChangeInputsearch.bind(this);
+    this.state = { itenSearch: '', onSearch: false };
+  }
+
+  onChangeInputsearch() { }
+
+  onChangeInputsearch(){ return null; }
+
+  render() {
+    const { onSearch } = this.state;
+    return (
+      <div>
+        <Header />
+        <div data-testid="page-search">
+          <div>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.onChangeInputsearch }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ onSearch }
+              onClick={ this.pesquisar }
+            >
+              Pesquisar
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
