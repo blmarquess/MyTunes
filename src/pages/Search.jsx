f874e999c21904990c8bbc2781fb1e1760b7025a
Bruno <blmarques.dev@gmail.com>
2021-11-15 05:50:25 -0300

    Add: Style with search imput and button

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import SearchIcon from '../components/IconSearch';
 
 export default class Search extends React.Component {
   constructor() {
@@ -27,22 +28,35 @@ export default class Search extends React.Component {
     return (
       <div>
         <Header />
-        <div data-testid="page-search">
-          <div>
+        <div
+          data-testid="page-search"
+          className="flex w-full justify-center items-center my-40"
+        >
+          <div
+            className="m-auto flex border-2 border-green-400 rounded-full pr-4 pl-0
+          bg-green-500 text-white font-bold"
+          >
             <input
               type="text"
               data-testid="search-artist-input"
+              className="py-2 pl-10 pr-20 border-0 rounded-l-3xl mr-0 text-green-900
+              outline-none focus:hover-green-700"
+              placeholder="Procure sua musica .."
               value={ itenSearch }
               onChange={ this.onChangeInputsearch }
             />
-            <button
-              type="button"
-              data-testid="search-artist-button"
-              disabled={ onSearch }
-              onClick={ this.pesquisar }
-            >
-              Pesquisar
-            </button>
+            <div className="flex items-center m-0">
+              <SearchIcon />
+              <button
+                className="ml-4"
+                type="button"
+                data-testid="search-artist-button"
+                disabled={ onSearch }
+                onClick={ this.pesquisar }
+              >
+                Pesquisar
+              </button>
+            </div>
           </div>
         </div>
       </div>
