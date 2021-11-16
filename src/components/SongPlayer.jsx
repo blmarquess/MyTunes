ae6a78a8deb953122a7b72627c87c23f65600585
Bruno <blmarques.dev@gmail.com>
2021-11-16 18:11:04 -0300

    Fix: Styled component Album:id

--- src/components/SongPlayer.jsx
+++ src/components/SongPlayer.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 const previewUrl = 'https://media.gettyimages.com/photos/an-artwork-issued-from-the-album-cover-the-wall-released-in-1979-is-picture-id862122228?s=612x612';
 
 export default () => (
-  <audio data-testid="audio-component" src={ previewUrl } controls download="false">
-    <track kind="captions" />
-    O seu navegador não suporta o elemento
-    <code>audio</code>
-  </audio>
+  <dvi className="shadow-xl w-full bg-black">
+    <audio data-testid="audio-component" src={ previewUrl } controls download="false">
+      <track kind="captions" />
+      O seu navegador não suporta o elemento
+      <code>audio</code>
+    </audio>
+  </dvi>
 );
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -7,16 +7,25 @@ const previewUrl = 'https://media.gettyimages.com/photos/an-artwork-issued-from-
 export default () => (
   <div>
     <Header />
-    <section data-testid="page-album" className="flex justify-around items-center mt-10">
+    <section data-testid="page-album" className="flex justify-center items-start mt-10">
       <section className="w-2/5 ml-10">
         <div className="w-72 h-96">
-          <img src={ previewUrl } alt="" className="object-cover h-72 w-full my-4" />
+          <img src={ previewUrl } alt="" className="object-cover h-72 w-full my-4 shadow-lx" />
           <span className="text-2xl font-semibold">Title</span>
           <p className="text-sm">Description</p>
         </div>
       </section>
       <section className="w-2/5">
         <div className="w-full">
+          <SongPlayer />
+          <SongPlayer />
+          <SongPlayer />
+          <SongPlayer />
+          <SongPlayer />
+          <SongPlayer />
+          <SongPlayer />
+          <SongPlayer />
+          <SongPlayer />
           <SongPlayer />
         </div>
       </section>
