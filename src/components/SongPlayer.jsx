2737e59ebf0785c88331fbaa4e7c3041778f95e4
Bruno <blmarques.dev@gmail.com>
2021-11-16 17:53:14 -0300

    Stylized: Album Page

--- src/components/SongPlayer.jsx
+++ src/components/SongPlayer.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+const previewUrl = 'https://media.gettyimages.com/photos/an-artwork-issued-from-the-album-cover-the-wall-released-in-1979-is-picture-id862122228?s=612x612';
+
 export default () => (
-  <audio data-testid="audio-component" src="{ previewUrl }" controls>
+  <audio data-testid="audio-component" src={ previewUrl } controls download="false">
     <track kind="captions" />
     O seu navegador não suporta o elemento
     <code>audio</code>
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import Header from '../components/Header';
+import SongPlayer from '../components/SongPlayer';
+
+const previewUrl = 'https://media.gettyimages.com/photos/an-artwork-issued-from-the-album-cover-the-wall-released-in-1979-is-picture-id862122228?s=612x612';
 
 export default () => (
   <div>
     <Header />
-    <section data-testid="page-album" />
+    <section data-testid="page-album" className="flex justify-around items-center mt-10">
+      <section className="w-2/5 ml-10">
+        <div className="w-72 h-96">
+          <img src={ previewUrl } alt="" className="object-cover h-72 w-full my-4" />
+          <span className="text-2xl font-semibold">Title</span>
+          <p className="text-sm">Description</p>
+        </div>
+      </section>
+      <section className="w-2/5">
+        <div className="w-full">
+          <SongPlayer />
+        </div>
+      </section>
+    </section>
   </div>);
