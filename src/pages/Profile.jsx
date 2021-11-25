33de6dd9295846694dfc7e45a0ba5bf73a4ab930
Bruno <blmarques.dev@gmail.com>
2021-11-24 22:23:50 -0300

    Feat: Page User Done with Requirement 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -7,10 +7,7 @@ export default class Profile extends React.Component {
   constructor() {
     super();
     this.state = {
-      name: '',
-      email: '',
-      image: '',
-      description: '',
+      user: {},
       loading: true,
     };
   }
@@ -19,16 +16,21 @@ export default class Profile extends React.Component {
     this.getProfileInfos();
   }
 
-  getProfileInfos = () => {
-    const { name, email, image, description } = getUser();
-    this.setState(() => ({ name, email, image, description }));
-    this.setState(() => ({ loading: false }));
+  getProfileInfos = async () => {
+    const user = await getUser();
+    this.setState(() => ({ user, loading: false }));
   }
 
   render() {
-    const { name, email, image, description, loading } = this.state;
+    const { user: {
+      name,
+      email,
+      description,
+      image,
+    }, loading } = this.state;
     return (
-      <div>
+      <div data-testid="page-profile">
+        <Header />
         {loading ? (
           <div className="flex p-4 space-x-2 pulse text-xl items-center">
             <div
@@ -39,20 +41,28 @@ export default class Profile extends React.Component {
           </div>)
 
           : (
-            <>
-              <Header />
-              <div data-testid="page-profile">
-                <section>
-                  <div data-testid="profile-image">{image}</div>
-                  <span data-testid="header-user-name">{name}</span>
-                  <span>{email}</span>
-                  <span>{description}</span>
+            <section>
+              <div>
+                <section
+                  className="container flex
+                justify-center items-center bg-gray-200"
+                >
+                  <div>
+                    <img
+                      data-testid="profile-image"
+                      src={ image }
+                      alt={ `Imagem do usuario ${name}` }
+                    />
+                  </div>
+                  <span>{ name }</span>
+                  <span>{ email }</span>
+                  <span>{ description }</span>
                   <Link to="/profile/edit">
                     <div>Editar perfil</div>
                   </Link>
                 </section>
               </div>
-            </>
+            </section>
           )}
       </div>
     );
